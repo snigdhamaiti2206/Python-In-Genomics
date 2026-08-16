8c88aab3b263ab705fdb3031ab1b15e1cfef4ce9
Snigdha Maiti <snigdhamaiti2207@gmail.com>
2026-08-16 19:12:17 +0530

    Code for pattern analysis

--- Codon Usage Pattern analysis/codon_usage.py
+++ Codon Usage Pattern analysis/codon_usage.py
@@ -2,11 +2,21 @@ from collections import Counter
 
 def read_sequence(file_path):
     with open(file_path, "r") as file:
-        sequence = file.read().strip().upper()
+        lines = file.readlines()
 
-    sequence = sequence.replace("\n", "")
+    sequence = ""
+
+    for line in lines:
+        line = line.strip()
+
+        if line.startswith(">"):
+            continue
+
+        sequence += line
+
+    sequence = sequence.upper()
+
     return sequence
-
 
 def count_codons(sequence):
     codons = []
@@ -14,29 +24,38 @@ def count_codons(sequence):
     for i in range(0, len(sequence) - 2, 3):
         codon = sequence[i:i+3]
 
-        if len(codon) == 3:
+        if all(base in "ATGC" for base in codon):
             codons.append(codon)
 
     return Counter(codons)
 
-
 def print_results(counter):
     total = sum(counter.values())
 
+    print("\n========== CODON USAGE ANALYSIS ==========")
     print(f"\nTotal Codons: {total}\n")
 
-    print("{:<8} {:<10} {:<10}".format("Codon", "Count", "Frequency"))
+    print("{:<8} {:<10} {:<12}".format(
+        "Codon", "Count", "Frequency"
+    ))
 
-    print("-" * 30)
+    print("-" * 32)
 
     for codon, count in sorted(counter.items()):
         frequency = count / total
-        print("{:<8} {:<10} {:.4f}".format(codon, count, frequency))
 
+        print("{:<8} {:<10} {:.4f}".format(
+            codon, count, frequency
+        ))
 
 if __name__ == "__main__":
 
-    sequence = read_sequence("sample_sequence.txt")
+    file_path = r"C:\Github Files\sample_sequence.txt"
+
+    sequence = read_sequence(file_path)
+
+    print("Sequence successfully loaded!")
+    print("Sequence length:", len(sequence), "bases")
 
     codon_counter = count_codons(sequence)
 
